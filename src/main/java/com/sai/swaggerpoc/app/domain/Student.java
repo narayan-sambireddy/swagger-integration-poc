5d1ef51612250d48da234f4a12a08cbfff8107fe
Narayan Sambi Reddy <narayan-sambireddy@outlook.com>
2018-08-06 23:27:23 +0530

    Create Student.java

---
+++ src/main/java/com/sai/swaggerpoc/app/domain/Student.java
@@ -0,0 +1,40 @@
+package com.sai.swaggerpoc.app.domain;
+
+public class Student {
+
+	private Long id;
+
+	private String name;
+
+	private Integer rank;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getRank() {
+		return rank;
+	}
+
+	public void setRank(Integer rank) {
+		this.rank = rank;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", rank=" + rank + "]";
+	}
+
+}
